b7eb6aaaf9c7da03c0ca5a0bcf6cf10dca9384e6
Беляев Егор Александрович <e.belyaev@univ.priv>
2021-09-16 13:31:21 +0300

    Edit output in new features

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!1");
+        System.out.println("Hello, World!\n");
+        System.out.println("New Feature!\n");
+
     }
 }
